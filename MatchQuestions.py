5a3022efac071e56ab8d8bd472b5525b5323468e
zhongshaofen <zhongshaofen@meituan.com>
2016-03-23 02:05:16 +0800

    匹配题目

--- MatchQuestions.py
+++ MatchQuestions.py
@@ -1,7 +1,18 @@
+#!/usr/bin/python2.7
+# coding=utf-8
+
 import re
 import codecs
 
 # ^\d.\s
 
-with codecs.open('./test', 'r') as readFile:
-	print(readFile.read().encode('utf-8'))
+with codecs.open('Chapters/Chapter60900', 'r', 'utf-8') as readFile:
+	matchForQuestions = re.compile(r'^\d.\s' + u"[\u0000-\ufffd]+" + r'(?=<\/dt>)')
+	matchForOptions = re.compile(r'\s[A-G]' + u'\uff0e')
+	for line in readFile.readlines(): 
+		matchQuestions = matchForQuestions.search(line, 0)
+		matchOptions = matchForOptions.search(line, 0)
+		if matchQuestions:
+			print(matchQuestions.group(0))
+		if matchOptions:
+			print(matchOptions.group(0))
