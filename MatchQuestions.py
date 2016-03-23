80458f6191964390654bcb4e5ab4eb03d72ee894
zhongshaofen <zhongshaofen@meituan.com>
2016-03-23 23:00:23 +0800

    输出一个文件题目

--- MatchQuestions.py
+++ MatchQuestions.py
@@ -4,15 +4,24 @@
 import re
 import codecs
 
-# ^\d.\s
-
 with codecs.open('Chapters/Chapter60900', 'r', 'utf-8') as readFile:
-	matchForQuestions = re.compile(r'^\d.\s' + u"[\u0000-\ufffd]+" + r'(?=<\/dt>)')
+	matchForQuestions = re.compile(r'^\d+.\s' + u"[\u0000-\ufffd]+" + r'(?=<\/dt>)')
 	matchForOptions = re.compile(r'\s[A-G]' + u'\uff0e')
-	for line in readFile.readlines(): 
+	lines = readFile.readlines()
+	optionLine = 0 #记录选项行号，下一行就是选项文字
+	mergeOptionString = ''
+	for i, line in enumerate(lines): 
 		matchQuestions = matchForQuestions.search(line, 0)
 		matchOptions = matchForOptions.search(line, 0)
 		if matchQuestions:
-			print(matchQuestions.group(0))
+			print('\n' + matchQuestions.group(0))
 		if matchOptions:
-			print(matchOptions.group(0))
+			optionLine = i + 1
+			mergeOptionString = line.strip()
+		if optionLine == i:
+			mergeOptionString += line.strip()
+			print(mergeOptionString)
+
+
+
+
